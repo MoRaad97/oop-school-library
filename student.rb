22a5c4d2a748029a6c9c3f8bae4aaa909b2ef590
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-08-30 18:40:52 +0300

    super variables

--- student.rb
+++ student.rb
@@ -4,8 +4,8 @@ class Student < Person
   attr_accessor :classroom
   attr_reader :age, :name, :parent_permission
 
-  def initialize(classroom)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name, parent_permission: true)
+    super(age, name, parent_permission: true)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require './person.rb'
 class Teacher < Person
   attr_accessor :specialization
   attr_reader :age, :name, :parent_permission
-  def initialize(specialization)
+  def initialize(specialization, age, name, parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
