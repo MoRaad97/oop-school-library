5aaad51e8b4a37a42b351fe569fa0a2a79ebaed0
Mohammed Raad Zahraw Al-Suwayyid <mohamedraad77@gmail.com>
2022-08-30 17:56:52 +0300

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require 'person'
+
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(specialization, _age, _name, _parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+
+end
