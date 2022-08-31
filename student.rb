47294ef05e6d0a7c3b143499c05c1850842cae5f
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-08-31 19:03:18 +0300

    Fix linters

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ class Student < Person
   attr_accessor :classroom
   attr_reader :age, :name, :parent_permission
 
-  def initialize(classroom, age, name, parent_permission: true)
+  def initialize(classroom,age, name, parent_permission: true)
     super(age, name, parent_permission: true)
     @classroom = classroom
   end
