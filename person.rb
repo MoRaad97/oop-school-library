b42803642aff988d9fce6764b57996bcb9e01d3d
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-09-01 13:24:22 +0300

    Add Classes "Nameable,BaseDecorator, CapitalizeDecorator,TrimmerDecorator"

--- person.rb
+++ person.rb
@@ -1,7 +1,9 @@
-class Person
+require './nameable.rb'
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
   def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -17,5 +19,9 @@ class Person
 
   def can_use_services?
     of_age?
+  end
+
+  def correct_name
+    @name
   end
 end
